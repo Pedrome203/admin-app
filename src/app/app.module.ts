cbcc36f1733ce6248ba10d80767fc7d3398462fd
Pedrome203 <47611247+Pedrome203@users.noreply.github.com>
2023-07-10 17:10:26 -0600

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InputOutputComponent } from './input-output/input-output.component';
+import { StatisticsComponent } from './input-output/statistics/statistics.component';
+import { DetailComponent } from './input-output/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    InputOutputComponent,
+    StatisticsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
